7288c26c79c8bb583b160c8a3a5fadfd42789855
Drew Robinson <drew.robinson@gmail.com>
2015-12-25 17:35:07 +0800

    Update error handlers

--- app.js
+++ app.js
@@ -10,12 +10,25 @@ app.enable('jsonp callback');
 app.use(morgan('combined'));
 
 app.use(express.static(__dirname + '/public'));
-app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.status(500);
-  res.render('error 500', {
-    error: err
-  });
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+app.use(function(err, req, res, next) {
+    console.log('error: ' + err);
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: err
+    }); 
 });
 
 var out = '';

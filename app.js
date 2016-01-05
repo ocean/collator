1098a13a918f876032ef5a20ae61fef37ee54dbe
Drew Robinson <drew.robinson@gmail.com>
2016-01-05 16:41:07 +0800

    Cleaned up both to comply with JSCS rules, and make clearer.
    Got rid of unneeded stuff.
    Brought (nonworking) ministerial section more into line with (working) Commerce news section (... then changed it all again).
    Express enabled to return JSONP by default, getting callback from "callback" query string if given.
    Changed cache clear instruction from optional named path parameter into query string instead, to make callback query string more straightforward too.
    Turned off Pantry parser instruction, relying on Pantry auto detect.

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
-var express = require('express'),
-  app = express(),
-  logger = require('morgan'),
-  Higgins = require('./higgins.js'),
-  pantry = require('pantry');
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var chalk = require('chalk');
+var Higgins = require('./higgins.js');
 
 app.enable('jsonp callback');
 
 // Morgan is a replacement for express.logger()
-app.use(logger('dev'));
+app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));
-
-var out = '';
-var result = {};
 
 // Possibly redirect root requests to one versioned endpoint by default, or an endpoint listing.
 //app.get('/', function (req, res) {
 //  res.redirect('/v1/s');
 //});
-//app.get('/s', function (req, res) {
-//  res.redirect('/v1/s');
-//});
 
 //Version 1 of the API :)
 app.get('/v1/ministerials/:clear?', function (req, res) {
-  // Ignore browser requests for Favicon
-//  if (req.url == "/favicon.ico") return false;
-  
   // URL of Ministerial Media Statements
   var url = 'https://www.mediastatements.wa.gov.au/Pages/Portfolios/Commerce.aspx';
-  
-  // Feed formatting - xml or json
+
+  // Feed formatting instructions for Pantry - xml or json
   var feedType = 'xml';
 
   // Get cache clear param from URL
-  var clear = req.params.clear;
-  
-  //Higgins.newsPlease()
+  var clear = false;
 
-  res.send('\r\r' + JSON.stringify(result, null, '  '));
-  res.end();
+  if (req.params.clear === 'clear-cache') {
+    clear = true;
+  }
+
+  Higgins.newsPlease(url, feedType, clear, function (news) {
+    res.send(news);
+    res.end();
+  });
 
   req.on('error', function (e) {
     console.log('request error: ' + e.message);
   });
 });
 
-app.get('/v1/commerce-news/:clear?', function (req, res) {
+app.get('/v1/commerce-news', function (req, res) {
   // URL of Commerce Media Releases ("Announcements")
   var url = 'http://www.commerce.wa.gov.au/taxonomy/term/182/feed';
-  
-  // Feed formatting - xml or json
+
+  // Feed formatting instructions for Pantry - xml or json
   var feedType = 'xml';
 
-  // Get cache clear param from URL
+  // Get cache clear query string from URL
   var clear = false;
-  
-  if (req.params.clear === 'clear-cache') {
+
+  if (req.query.clearCache === 'true') {
     clear = true;
   }
-  
+//  console.log(chalk.magenta('Cache clear status is: %s'), clear);
+//  console.dir(req.query);
+
   Higgins.newsPlease(url, feedType, clear, function (news) {
-    res.send(news);
+//    console.dir(news.$, { colors: 'true' });
+//    console.dir(news.channel[0].item[5], { colors: 'true', depth: 5 });
+//    console.log('Number of items: ' + news.channel[0].item.length);
+//    res.type('json');
+//    res.send(news);
+    res.jsonp(news);
     res.end();
   });
-  
+
   req.on('error', function (e) {
     console.log('request error: ' + e.message);
   });
@@ -87,7 +89,7 @@ app.use(function(err, req, res, next) {
     res.send({
         message: err.message,
         error: err
-    }); 
+    });
 });
 */
 
--- higgins.js
+++ higgins.js
@@ -1,23 +1,31 @@
-var Higgins = function () {};
-
 var pantry = require('pantry');
+var util = require('util');
+
+var Higgins = function () {};
 
 Higgins.prototype.newsPlease = function (url, feedType, clear, callback) {
   pantry.configure({
     maxLife: 600,
-    parser: 'xml'
+//    parser: feedType,
+    xmlOptions: {
+      explicitRoot: false,
+//      explicitChildren: true,
+//      childKey: 'article',
+    },
   });
-  
+
   pantry.fetch({
-    uri: url
+    uri: url,
   }, function (error, data) {
-    console.log('Fetching: ' + url);
+//    console.log('Fetching: ' + url);
     if (error) {
       console.log('pantry error: ' + error);
     }
-    callback(JSON.stringify(data, null, 2));
+//    callback(JSON.stringify(data, null, 2));
+    callback(data);
+//    console.dir(util.inspect(data, false, null));
   });
-  
+
 };
 
 module.exports = new Higgins();

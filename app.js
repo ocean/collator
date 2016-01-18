d1badd9cf1cfc772e8d5b03b8ab2045878f3b5e3
Drew Robinson <drew.robinson@gmail.com>
2016-01-18 16:42:20 +0800

    Added CodeClimate test to Travis, fixed some things ESLint is complaining about.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 var chalk = require('chalk');
-var cheerio = require('cheerio');
+// var cheerio = require('cheerio');
 var cors = require('cors');
 var higgins = require('./higgins.js');
 var morgan = require('morgan');
@@ -16,9 +16,9 @@ app.use(express.static(__dirname + '/public'));
 console.log(chalk.red('Node environment: ') + chalk.cyan(app.get('env')));
 
 // Enable CORS for some domains
-var corsWhitelist = ['http://203.33.230.66', 'http://www.commerce.wa.gov.au', 'https://www.commerce.wa.gov.au'];
+// var corsWhitelist = ['http://203.33.230.66', 'http://www.commerce.wa.gov.au', 'https://www.commerce.wa.gov.au'];
 var corsOptions = {
-  origin: function (origin, callback) {
+  origin: function corsOriginCheck(origin, callback) {
 //    var originIsWhitelisted = corsWhitelist.indexOf(origin) !== -1;
 //    callback(null, originIsWhitelisted);
     // Setting to "true" to whitelist all origins for development
@@ -29,7 +29,7 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // Redirect root requests to a nice index page.
-app.get('/', function (req, res) {
+app.get('/', function getRoot(req, res) {
   res.redirect('/public/index.html');
 });
 
@@ -40,7 +40,7 @@ app.get('/', function (req, res) {
 // 'v1' for Version 1 of the API :)
 
 // Commerce Media Releases from the 'Announcements' section of the website
-app.get('/v1/commerce-news', function (req, res, next) {
+app.get('/v1/commerce-news', function getCommerceNews(req, res, next) {
   // URL of Commerce Media Releases ("Announcements")
 //  var url = 'http://www.commerce.wa.gov.au/taxonomy/term/182/feed';
   var url = 'http://www.commerce.wa.gov.au/announcements/182/all/feed';
@@ -57,7 +57,7 @@ app.get('/v1/commerce-news', function (req, res, next) {
 //  console.log(chalk.magenta('Cache clear status is: %s'), clear);
 //  console.dir(req.query);
 
-  higgins.newsPlease(url, feedType, forceRefresh, function (error, news) {
+  higgins.newsPlease(url, feedType, forceRefresh, function newsCB(error, news) {
     if (error) { return next(error); }
 //    console.dir(news.$, { colors: 'true' });
 //    console.dir(news.channel[0].item[5], { colors: 'true', depth: 5 });
@@ -68,7 +68,7 @@ app.get('/v1/commerce-news', function (req, res, next) {
   });
 });
 
-app.get('/v1/ministerials', function (req, res, next) {
+app.get('/v1/ministerials', function getMinisterials(req, res, next) {
   // URL of Ministerial Media Statements built by this app
   var url = '/v1/build/ministerials';
 
@@ -82,7 +82,7 @@ app.get('/v1/ministerials', function (req, res, next) {
     forceRefresh = true;
   }
 
-  higgins.newsPlease(url, feedType, forceRefresh, function (error, news) {
+  higgins.newsPlease(url, feedType, forceRefresh, function ministerialsCB(error, news) {
     if (error) { return next(error); }
 //    res.send(news);
     res.jsonp(news);
@@ -93,7 +93,7 @@ app.get('/v1/ministerials', function (req, res, next) {
 // Endpoints for building sets of data
 // ===================================
 
-app.get('/v1/build/ministerials', function (req, res, next) {
+app.get('/v1/build/ministerials', function buildMinisterials(req, res, next) {
 //  console.log('Request: ' + req);
   // URL of Ministerial Media Statements start page
   var startUrl = 'https://www.mediastatements.wa.gov.au/Pages/Portfolios/Commerce.aspx';
@@ -118,19 +118,19 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function errorHandler(err, req, res, next) {
   res.status(err.status || 500).send('Error: ' + err.message);
   console.error('Error stack: \n' + err.stack);
 });
 
 // catch 404 as a final thing
-app.use(function (req, res, next) {
+app.use(function notFoundHandler(req, res, next) {
   res.status(404).send('404 Not Found');
   console.dir(req);
 });
 
 // port setup with env var for Heroku
 var port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function appListen() {
   console.log('Listening on ' + port);
 });

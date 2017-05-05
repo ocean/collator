e9253e52918a6e806feb42e0a44ba14bd6f6daf6
Drew Robinson <drew.robinson@gmail.com>
2017-05-05 17:19:25 +0800

    Non-working hapi rewrite

--- app.js
+++ app.js
@@ -1,139 +1,207 @@
-var express = require('express');
-var app = express();
+'use strict';
 
-var chalk = require('chalk');
-// var cheerio = require('cheerio');
-var cors = require('cors');
-var collator = require('./collator.js');
-var morgan = require('morgan');
-var path = require('path');
+const Hapi = require('hapi');
+const Good = require('good');
+const Collator = require('./collator.js');
 
-// Morgan is a replacement for express.logger()
-app.use(morgan('dev'));
+// const collator = require('./collator.js');
+
+const server = new Hapi.Server();
+
+// port setup with env var for hosting
+server.connection({
+  port: process.env.PORT || 8000,
+  host: 'localhost'
+});
+
+// static file handler for public directory
+server.register(require('inert'), (err) => {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/{static*}',
+    handler: {
+      directory: {
+        path: 'public',
+        index: true
+      }
+    }
+  })
+});
+
+server.route([
+  {
+    method: 'GET',
+    path: '/test',
+    handler: function (request, reply) {
+      reply({
+        message: 'Hello there!'
+      })
+    }
+  },
+  {
+    method: 'GET',
+    path: '/v1/{path*}',
+    handler: function (request, reply) {
+      let results = Collator.get(request);
+      // typeof results;
+      reply(results);
+    }
+  }
+]);
+
+server.register({
+  register: Good,
+  options: {
+    reporters: {
+      console: [{
+        module: 'good-squeeze',
+        name: 'Squeeze',
+        args: [{
+          response: '*',
+          log: '*'
+        }]
+      }, {
+        module: 'good-console'
+      }, 'stdout']
+    }
+  }
+}, (err) => {
+  if (err) {
+    throw err;
+  }
+
+  server.start((err) => {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+    server.log('info', `Server running at ${server.info.uri}`);
+  });
+});
+
 
 // Serve anything in public as static files
-app.use(express.static(path.join(__dirname, 'public')));
-
-console.log(chalk.red('Node environment: ') + chalk.cyan(app.get('env')));
-
-// Enable CORS for some domains
-// var corsWhitelist = ['http://203.33.230.66', 'http://www.commerce.wa.gov.au', 'https://www.commerce.wa.gov.au'];
-var corsOptions = { // eslint-disable-line
-  origin: function corsOriginCheck(origin, callback) {
-//    var originIsWhitelisted = corsWhitelist.indexOf(origin) !== -1;
-//    callback(null, originIsWhitelisted);
-    // Setting to "true" to whitelist all origins for development
-    callback(null, true);
-    console.log(chalk.red('Origin is: ') + chalk.cyan(origin));
-  },
-};
-app.use(cors(corsOptions));
-
-// Redirect root requests to a nice index page.
-app.get('/', function getRoot(req, res) {
-  res.redirect('/public/index.html');
-});
-
-// ========================================
-// Public endpoints for returning JSON data
-// ========================================
-
-// 'v1' for Version 1 of the API :)
-
-// Commerce Media Releases from the 'Announcements' section of the website
-app.get('/v1/commerce-news', function getCommerceNews(req, res, next) {
-  // URL of Commerce Media Releases ("Announcements")
-//  var url = 'http://www.commerce.wa.gov.au/taxonomy/term/182/feed';
-  var url = 'http://www.commerce.wa.gov.au/announcements/182/all/feed';
-
-  // Feed formatting instructions for Pantry - xml or json
-  var feedType = 'xml';
-
-  // Get cache clear query string from URL
-  var forceRefresh = false;
-
-  if (req.query.forceRefresh === 'true') {
-    forceRefresh = true;
-  }
-//  console.log(chalk.magenta('Cache clear status is: %s'), clear);
-//  console.dir(req.query);
-
-  collator.newsPlease(url, feedType, forceRefresh, function newsCB(error, news) {
-    if (error) { return next(error); }
-//    console.dir(news.$, { colors: 'true' });
-//    console.dir(news.channel[0].item[5], { colors: 'true', depth: 5 });
-//    console.log('Number of items: ' + news.channel[0].item.length);
-//    res.type('json');
-    res.jsonp(news);
-    return true;
-  });
-});
-
-app.get('/v1/ministerials', function getMinisterials(req, res, next) {
-  // URL of Ministerial Media Statements built by this app
-  var url = '/v1/build/ministerials';
-
-  // Feed formatting instructions for Pantry - xml or json
-  var feedType = 'json';
-
-  // Get cache clear param from URL
-  var forceRefresh = false;
-
-  if (req.query.forceRefresh === 'true') {
-    forceRefresh = true;
-  }
-
-  collator.newsPlease(url, feedType, forceRefresh, function ministerialsCB(error, news) {
-    if (error) { return next(error); }
-//    res.send(news);
-    res.jsonp(news);
-    return true;
-  });
-});
-
-// ===================================
-// Endpoints for building sets of data
-// ===================================
-
-app.get('/v1/build/ministerials', function buildMinisterials(req, res) {
-//  console.log('Request: ' + req);
-  // URL of Ministerial Media Statements start page
-  // var startUrl = 'https://www.mediastatements.wa.gov.au/Pages/Portfolios/Commerce.aspx';
-
-  res.jsonp(JSON.stringify({ result: 'This is where the data goes.' }));
-});
-
-// ===================================
-// All the error things go at the end.
-// ===================================
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function devError(err, req, res, next) {
-    res.status(err.status || 500).send(err.stack);
-    console.error('Error stack: \n' + err.stack);
-//    console.dir(req);
-    next(err);
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function errorHandler(err, req, res, next) {
-  res.status(err.status || 500).send('Error: ' + err.message);
-  console.error('Error stack: \n' + err.stack);
-  next(err);
-});
-
-// catch 404 as a final thing
-app.use(function notFoundHandler(req, res) {
-  res.status(404).send('404 Not Found');
-  console.dir(req);
-});
-
-// port setup with env var for Heroku
-var port = process.env.PORT || 3000; // eslint-disable-line
-app.listen(port, function appListen() {
-  console.log('Listening on ' + port);
-});
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// console.log(chalk.red('Node environment: ') + chalk.cyan(app.get('env')));
+//
+// // Enable CORS for some domains
+// // var corsWhitelist = ['http://203.33.230.66', 'http://www.commerce.wa.gov.au', 'https://www.commerce.wa.gov.au'];
+// var corsOptions = { // eslint-disable-line
+//   origin: function corsOriginCheck(origin, callback) {
+// //    var originIsWhitelisted = corsWhitelist.indexOf(origin) !== -1;
+// //    callback(null, originIsWhitelisted);
+//     // Setting to "true" to whitelist all origins for development
+//     callback(null, true);
+//     console.log(chalk.red('Origin is: ') + chalk.cyan(origin));
+//   },
+// };
+// app.use(cors(corsOptions));
+//
+// // Redirect root requests to a nice index page.
+// app.get('/', function getRoot(req, res) {
+//   res.redirect('/public/index.html');
+// });
+//
+// // ========================================
+// // Public endpoints for returning JSON data
+// // ========================================
+//
+// // 'v1' for Version 1 of the API :)
+//
+// // Commerce Media Releases from the 'Announcements' section of the website
+// app.get('/v1/commerce-news', function getCommerceNews(req, res, next) {
+//   // URL of Commerce Media Releases ("Announcements")
+// //  var url = 'http://www.commerce.wa.gov.au/taxonomy/term/182/feed';
+//   var url = 'http://www.commerce.wa.gov.au/announcements/182/all/feed';
+//
+//   // Feed formatting instructions for Pantry - xml or json
+//   var feedType = 'xml';
+//
+//   // Get cache clear query string from URL
+//   var forceRefresh = false;
+//
+//   if (req.query.forceRefresh === 'true') {
+//     forceRefresh = true;
+//   }
+// //  console.log(chalk.magenta('Cache clear status is: %s'), clear);
+// //  console.dir(req.query);
+//
+//   collator.newsPlease(url, feedType, forceRefresh, function newsCB(error, news) {
+//     if (error) { return next(error); }
+// //    console.dir(news.$, { colors: 'true' });
+// //    console.dir(news.channel[0].item[5], { colors: 'true', depth: 5 });
+// //    console.log('Number of items: ' + news.channel[0].item.length);
+// //    res.type('json');
+//     res.jsonp(news);
+//     return true;
+//   });
+// });
+//
+// app.get('/v1/ministerials', function getMinisterials(req, res, next) {
+//   // URL of Ministerial Media Statements built by this app
+//   var url = '/v1/build/ministerials';
+//
+//   // Feed formatting instructions for Pantry - xml or json
+//   var feedType = 'json';
+//
+//   // Get cache clear param from URL
+//   var forceRefresh = false;
+//
+//   if (req.query.forceRefresh === 'true') {
+//     forceRefresh = true;
+//   }
+//
+//   collator.newsPlease(url, feedType, forceRefresh, function ministerialsCB(error, news) {
+//     if (error) { return next(error); }
+// //    res.send(news);
+//     res.jsonp(news);
+//     return true;
+//   });
+// });
+//
+// // ===================================
+// // Endpoints for building sets of data
+// // ===================================
+//
+// app.get('/v1/build/ministerials', function buildMinisterials(req, res) {
+// //  console.log('Request: ' + req);
+//   // URL of Ministerial Media Statements start page
+//   // var startUrl = 'https://www.mediastatements.wa.gov.au/Pages/Portfolios/Commerce.aspx';
+//
+//   res.jsonp(JSON.stringify({ result: 'This is where the data goes.' }));
+// });
+//
+// // ===================================
+// // All the error things go at the end.
+// // ===================================
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function devError(err, req, res, next) {
+//     res.status(err.status || 500).send(err.stack);
+//     console.error('Error stack: \n' + err.stack);
+// //    console.dir(req);
+//     next(err);
+//   });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function errorHandler(err, req, res, next) {
+//   res.status(err.status || 500).send('Error: ' + err.message);
+//   console.error('Error stack: \n' + err.stack);
+//   next(err);
+// });
+//
+// // catch 404 as a final thing
+// app.use(function notFoundHandler(req, res) {
+//   res.status(404).send('404 Not Found');
+//   console.dir(req);
+// });
--- collator.js
+++ collator.js
@@ -1,49 +1,64 @@
-var pantry = require('pantry');
+// const pantry = require('pantry');
 // var util = require('util');
 
-var Collator = function setup() {};
+// const Collator = function setup() {};
 
-Collator.prototype.newsPlease = function hereIsTheNews(url, feedType, forceRefresh, callback) {
-  var fetchLocation = url;
-  pantry.configure({
-    maxLife: 600,
-//    parser: feedType,
-    xmlOptions: {
-      explicitRoot: false,
-//      explicitChildren: true,
-//      childKey: 'article',
-    },
-  });
+module.exports = {
 
-  // Check if requested URL is local (relative), if so make it valid (absolute)
-  if (fetchLocation.indexOf('http') === -1) {
-    // URL has no http, so is relative
-    if (process.env.HEROKU_URL) {
-      // if on Heroku, add the app URL
-      fetchLocation = process.env.HEROKU_URL + fetchLocation;
-    } else {
-      // if on local, add default Node URL
-      fetchLocation = 'http://localhost:3000' + fetchLocation;
+  get: (request, reply) => {
+    "use strict";
+    // console.dir(request);
+    console.log(request.params.path);
+    let ret = setTimeout(function () {
+      console.log('this took 5s');
+      return 'this thing took 5s'
+    }, 5000);
+    // return 'hi there';
+    return ret;
+  },
+
+  newsPlease: function hereIsTheNews(url, feedType, forceRefresh, callback) {
+    var fetchLocation = url;
+    pantry.configure({
+      maxLife: 600,
+  //    parser: feedType,
+      xmlOptions: {
+        explicitRoot: false,
+  //      explicitChildren: true,
+  //      childKey: 'article',
+      },
+    });
+
+    // Check if requested URL is local (relative), if so make it valid (absolute)
+    if (fetchLocation.indexOf('http') === -1) {
+      // URL has no http, so is relative
+      if (process.env.HEROKU_URL) {
+        // if on Heroku, add the app URL
+        fetchLocation = process.env.HEROKU_URL + fetchLocation;
+      } else {
+        // if on local, add default Node URL
+        fetchLocation = 'http://localhost:3000' + fetchLocation;
+      }
     }
-  }
-  console.log('URL is: ' + fetchLocation);
+    console.log('URL is: ' + fetchLocation);
 
-  if (forceRefresh) {
-    pantry.remove(fetchLocation);
-    console.log('Removed cached copy of: ' + fetchLocation);
-  }
-
-  pantry.fetch({
-    uri: fetchLocation,
-  }, function fetchThings(error, data) {
-//    console.log('Fetching: ' + url);
-    if (error) {
-      console.log('pantry error: ' + error);
+    if (forceRefresh) {
+      pantry.remove(fetchLocation);
+      console.log('Removed cached copy of: ' + fetchLocation);
     }
-//    callback(JSON.stringify(data, null, 2));
-    callback(error, data);
-//    console.dir(util.inspect(data, false, null));
-  });
+
+    pantry.fetch({
+      uri: fetchLocation,
+    }, function fetchThings(error, data) {
+  //    console.log('Fetching: ' + url);
+      if (error) {
+        console.log('pantry error: ' + error);
+      }
+  //    callback(JSON.stringify(data, null, 2));
+      callback(error, data);
+  //    console.dir(util.inspect(data, false, null));
+    });
+  }
 };
 
-module.exports = new Collator();
+// module.exports = new Collator();

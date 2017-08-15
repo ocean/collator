0dc92b36311ac4e4592a80f05bca60d2cf75dedd
Drew Robinson <drew.robinson@gmail.com>
2017-08-15 17:57:43 +0800

    Got combined feed working, though it's a bit slowish

--- src/handlers/collator.js
+++ src/handlers/collator.js
@@ -1,39 +1,58 @@
-import req from 'request';
-// import CommerceNews from './commerce-news';
-// import GovernmentNews from './government-news';
-// import Ministerials from './ministerials';
-// import Tweets from './tweets';
+import goodGuyHttp from 'good-guy-http';
 
-exports.combinedNews = function collateAllNews(request, reply) {
+const goodGuy = goodGuyHttp({
+  // defaultCaching: {
+  //   timeToLive: 300000,
+  // },
+  // headers: {
+  //   'User-Agent': 'Department of Commerce Intranet - request',
+  // },
+  // proxy: process.env.HTTP_PROXY,
+  timeout: 15000,
+});
+
+exports.combinedNews = async function collateAllNews(request, reply) {
   // console.dir(request);
-  console.dir(request.connection.server.info);
+  // console.dir(request.connection.server.info);
   // console.dir(server.info);
   const endpointProto = request.connection.server.info.protocol;
-  console.log(endpointProto);
+  // console.log(endpointProto);
   const endpointPort = request.connection.server.info.port;
-  console.log(endpointPort);
+  // console.log(endpointPort);
   const checkHost = request.connection.server.info.host;
-  console.log(checkHost);
+  // console.log(checkHost);
   let endpointHost = '';
   if (checkHost.indexOf('local') > 0 ) {
-    console.log('localhost fired');
+    // console.log('localhost fired');
     endpointHost = 'localhost';
   } else {
-    console.log('remote host fired');
+    // console.log('remote host fired');
     endpointHost = checkHost;
   };
   // const endpointUrl = 'http://localhost:3000';
   const endpointUrl = `${endpointProto}://${endpointHost}:${endpointPort}`;
-  console.log('endpoint url:', endpointUrl);
+  // console.log('endpoint url:', endpointUrl);
   let news = [];
-  req(`${endpointUrl}/api/v1/statements/commerce`, (error, response, body) => {
-    if (error) {
-      console.log(error);
-    }
-    // console.log(body);
-    news.push(JSON.parse(body));
-    console.dir(news);
-  });
-  console.dir(news);
-  reply('testing');
+  const commerceNews = await goodGuy(`${endpointUrl}/api/v1/statements/commerce`);
+  news.push(JSON.parse(commerceNews.body.toString()));
+  const ministerials = await goodGuy(`${endpointUrl}/api/v1/statements/ministerials`);
+  news.push(JSON.parse(ministerials.body.toString()));
+  const governmentNews = await goodGuy(`${endpointUrl}/api/v1/statements/government`);
+  news.push(JSON.parse(governmentNews.body.toString()));
+  const dmirsTweets = await goodGuy(`${endpointUrl}/api/v1/tweets/dmirs`);
+  news.push(JSON.parse(dmirsTweets.body.toString()));
+  const combined = news.concat(
+    JSON.parse(commerceNews.body.toString()),
+    JSON.parse(ministerials.body.toString()),
+    JSON.parse(governmentNews.body.toString()),
+    JSON.parse(dmirsTweets.body.toString())
+  );
+  const flattened = combined.reduce((a, b) => {
+    return a.concat(b);
+  }, []);
+  console.log('items count =', flattened.length);
+  // console.dir(news);
+  // reply('testing');
+  // reply(combined);
+  reply(flattened);
 };

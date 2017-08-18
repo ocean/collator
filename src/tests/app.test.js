2b8c5618affa9d3d8c2ff6093af6b41e25e90e12
Drew Robinson <drew.robinson@gmail.com>
2017-08-18 17:23:04 +0800

    Begin adding some API tests

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,9 +1,23 @@
 /* Simple app tests with ava. */
 
 import test from 'ava';
+import request from 'supertest';
 
 import server from '../app';
 
+// const app = server();
+
 test('did the app load', (t) => {
   t.truthy(server, 'Hapi server loaded');
+  t.log(server);
+});
+
+// console.dir(server);
+
+test('check the homepage loads', async (t) => {
+  t.plan(1);
+  const res = await request(server)
+    .get('/');
+
+  t.is(res.status, 200);
 });

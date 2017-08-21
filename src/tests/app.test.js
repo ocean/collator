819a70547bac1bfff5bea7745a1de0d9af3aa37b
Drew Robinson <drew.robinson@gmail.com>
2017-08-21 17:16:11 +0800

    Minor Babel config improvements for setup of some tests

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -4,20 +4,32 @@ import test from 'ava';
 // import request from 'supertest';
 
 import server from '../app';
+import routes from '../routes';
 
 // const app = server();
 
+// test.beforeEach((t) => {
+//   t.context.server = server;
+// });
+
 test('did the app load', (t) => {
   t.truthy(server, 'Hapi server loaded');
-  t.log(server);
+  // t.log(server);
+});
+
+test('did the routes load', (t) => {
+  t.truthy(routes, 'Hapi routes loaded');
+  // t.log(console.dir(routes[0]));
 });
 
 // console.dir(server);
 
 // test('check the homepage loads', async (t) => {
 //   t.plan(1);
+//   t.log(console.dir(server));
 //   const res = await request(server)
 //     .get('/');
 
+//   t.log('do we get here?');
 //   t.is(res.status, 200);
 // });

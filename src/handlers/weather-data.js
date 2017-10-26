42f670dd7f08a676ce4f24b2a9856c252c71f76b
Drew Robinson <drew.robinson@gmail.com>
2017-10-27 00:26:49 +0800

    Update npm packages and clean up linting in weather-data file

--- src/handlers/weather-data.js
+++ src/handlers/weather-data.js
@@ -1,37 +1,33 @@
-import fs from "fs";
-import goodGuyHttp from "good-guy-http";
-import he from "he";
-import moment from "moment";
-import xml2js from "xml2js";
-import url from "url";
+import fs from 'fs';
+import goodGuyHttp from 'good-guy-http';
+import moment from 'moment';
+import url from 'url';
 
-let caFile = "";
+let caFile = '';
 
-if (process.env.NODE_ENV === "production") {
-  caFile = fs.readFileSync("/etc/pki/tls/cert.pem");
+if (process.env.NODE_ENV === 'production') {
+  caFile = fs.readFileSync('/etc/pki/tls/cert.pem');
 }
 
 const goodGuy = goodGuyHttp({
   forceCaching: {
-    timeToLive: 30000
+    timeToLive: 30000,
   },
   ca: caFile,
   headers: {
-    "User-Agent": "Department of Commerce Intranet - request"
+    'User-Agent': 'Department of Commerce Intranet - request',
   },
   json: true,
   proxy: process.env.HTTP_PROXY,
-  timeout: 15000
+  timeout: 15000,
 });
 
-
 exports.getForecast = async function getWeather(request, reply) {
-
   const { location } = request.params;
 
   const locations = {
-    perth: "2063523",
-    cannington: "2075024"
+    perth: '2063523',
+    cannington: '2075024',
   };
 
   const openWeatherMapEndPoint = url.format({
@@ -40,31 +36,26 @@ exports.getForecast = async function getWeather(request, reply) {
     pathname: 'data/2.5/forecast',
     query: {
       id: locations[location],
-      mode: "json",
-      appid: process.env.OPENWEATHERMAP_API_KEY
+      mode: 'json',
+      appid: process.env.OPENWEATHERMAP_API_KEY,
     },
   });
-
 
   try {
     // Get the featured status from the request parameter
     // const { featured } = request.params;
     const newsResponse = await goodGuy(openWeatherMapEndPoint);
 
-    const forecasts = newsResponse.body.list.map((forecast) => {
-      return {
-        dateTime: moment.unix(forecast.dt).toISOString(),
-        humidity: forecast.main.humidity,
-        tempurate: forecast.main.temp,
-        weather: forecast.weather[0].main,
-        weather_id: forecast.weather[0].id,
-      };
-    });
-
+    const forecasts = newsResponse.body.list.map(forecast => ({
+      dateTime: moment.unix(forecast.dt).toISOString(),
+      humidity: forecast.main.humidity,
+      tempurate: forecast.main.temp,
+      weather: forecast.weather[0].main,
+      weather_id: forecast.weather[0].id,
+    }));
     reply(forecasts);
-
   } catch (error) {
-    console.error("Error fetching weather forecast.", error);
+    console.error('Error fetching weather forecast.', error);
     reply(error.response.body);
   }
 };

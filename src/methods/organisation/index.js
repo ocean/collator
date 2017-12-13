c16208e037ae59cc422b184df73e4b723e406dc4
Cyrus Eftos <cyruseftos@gmail.com>
2017-12-13 17:13:53 +0800

    Organisation structure. Takes awhile to generate so it probably needs refactoring but a bit of caching wouldnt hurt either

--- src/methods/organisation/index.js
+++ src/methods/organisation/index.js
@@ -1,9 +1,30 @@
 import connection from "../../config/database";
+import _ from "lodash";
+
 module.exports.register = (server, options, next) => {
   async function getOrganisation(next) {
     try {
-      return next(null, { org: "soon" });
+      let structure = {};
+
+      const organisation = await connection
+        .table("employees")
+        .filter(doc => {
+          return doc("div").ne("");
+        })
+        .pluck("grp", "div", "sect", "bran");
+
+      const filtered = Array.from(
+        new Set(organisation.map(JSON.stringify))
+      ).map(JSON.parse);
+
+      filtered.map(obj => {
+        _.merge(structure, { [obj.grp]: { [obj.div]: { [obj.bran]: [] } } });
+        structure[obj.grp][obj.div][obj.bran].push(obj.sect);
+      });
+
+      return next(null, structure);
     } catch (error) {
+      console.log(error);
       next(error);
     }
   }

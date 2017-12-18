8f0e36868cc4033d698bf9d047967d5639c54436
Drew Robinson <drew.robinson@gmail.com>
2017-12-18 13:27:06 +0800

    ESLint fixes

--- src/config/routes/collator.js
+++ src/config/routes/collator.js
@@ -1,142 +1,142 @@
-import Joi from "joi";
+import Joi from 'joi';
 // import Collator from '../../handlers/collator';
-import CommerceNews from "../../handlers/commerce-news";
-import GovernmentNews from "../../handlers/government-news";
-import IntranetNews from "../../handlers/intranet-news";
-import Ministerials from "../../handlers/ministerials";
-import TransportData from "../../handlers/transport-data";
-import Tweets from "../../handlers/tweets";
-import WeatherData from "../../handlers/weather-data";
+import CommerceNews from '../../handlers/commerce-news';
+import GovernmentNews from '../../handlers/government-news';
+import IntranetNews from '../../handlers/intranet-news';
+import Ministerials from '../../handlers/ministerials';
+import TransportData from '../../handlers/transport-data';
+import Tweets from '../../handlers/tweets';
+import WeatherData from '../../handlers/weather-data';
 
 module.exports = [
   {
-    method: "GET",
-    path: "/api/v1/statements/commerce",
+    method: 'GET',
+    path: '/api/v1/statements/commerce',
     config: {
       handler: CommerceNews.getStatements,
-      description: "Commerce",
-      notes: "Commerce media statements from the Commerce site.",
-      tags: ["api", "Statements"]
-    }
+      description: 'Commerce',
+      notes: 'Commerce media statements from the Commerce site.',
+      tags: ['api', 'Statements'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/intranet/news/{featured?}",
+    method: 'GET',
+    path: '/api/v1/intranet/news/{featured?}',
     config: {
       handler: IntranetNews.getIntranetNews,
-      description: "Intranet News",
-      notes: "News items (currently all of them) from the new Intranet.",
-      tags: ["api", "Intranet"]
-    }
+      description: 'Intranet News',
+      notes: 'News items (currently all of them) from the new Intranet.',
+      tags: ['api', 'Intranet'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/intranet/news/type/{newsType}/{featured?}",
+    method: 'GET',
+    path: '/api/v1/intranet/news/type/{newsType}/{featured?}',
     config: {
       handler: IntranetNews.getIntranetNewsByType,
-      description: "Intranet News",
-      notes: "News items, filtered by type, from the new Intranet.",
-      tags: ["api", "Intranet"]
-    }
+      description: 'Intranet News',
+      notes: 'News items, filtered by type, from the new Intranet.',
+      tags: ['api', 'Intranet'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/statements/ministerials",
+    method: 'GET',
+    path: '/api/v1/statements/ministerials',
     config: {
       handler: Ministerials.getMinisterials,
-      description: "Ministerials",
+      description: 'Ministerials',
       notes:
-        "Ministerial media statements from the WA Govt Media Statements site - the Mines and Petroleum and Commerce and Industrial Relations portfolios.",
-      tags: ["api", "Statements"]
-    }
+        'Ministerial media statements from the WA Govt Media Statements site - the Mines and Petroleum and Commerce and Industrial Relations portfolios.',
+      tags: ['api', 'Statements'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/statements/government",
+    method: 'GET',
+    path: '/api/v1/statements/government',
     config: {
       handler: GovernmentNews.getStatements,
-      description: "Government",
-      notes: "All Government media statements from the Media Statements site.",
-      tags: ["api", "Statements"]
-    }
+      description: 'Government',
+      notes: 'All Government media statements from the Media Statements site.',
+      tags: ['api', 'Statements'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/tweets/{list}",
+    method: 'GET',
+    path: '/api/v1/tweets/{list}',
     config: {
       handler: Tweets.getTweets,
-      description: "Tweets",
+      description: 'Tweets',
       notes:
-        "Twitter updates from either @DMIRS_WA and related accounts or Perth transport information, including @Transperth and @Perth_Traffic.",
-      tags: ["api", "Tweets"]
-    }
+        'Twitter updates from either @DMIRS_WA and related accounts or Perth transport information, including @Transperth and @Perth_Traffic.',
+      tags: ['api', 'Tweets'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/data/transport/departures/{location}",
+    method: 'GET',
+    path: '/api/v1/data/transport/departures/{location}',
     handler: TransportData.getDepartures,
     config: {
       cache: {
-        expiresIn: 30000
+        expiresIn: 30000,
       },
       validate: {
         params: {
           location: Joi.string()
-            .valid("baldivis", "cannington", "carlisle", "east-perth", "perth")
-            .required()
-        }
+            .valid('baldivis', 'cannington', 'carlisle', 'east-perth', 'perth')
+            .required(),
+        },
       },
-      description: "Departures",
-      notes: "Train departures information from Transperth live train times.",
-      tags: ["api", "Transport"]
-    }
+      description: 'Departures',
+      notes: 'Train departures information from Transperth live train times.',
+      tags: ['api', 'Transport'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/data/transport/updates",
+    method: 'GET',
+    path: '/api/v1/data/transport/updates',
     config: {
       handler: TransportData.getUpdates,
-      description: "Updates",
-      notes: "Train service updates from Transperth train updates.",
-      tags: ["api", "Transport"]      
-    }
+      description: 'Updates',
+      notes: 'Train service updates from Transperth train updates.',
+      tags: ['api', 'Transport'],
+    },
   },
   {
-    method: "GET",
-    path: "/api/v1/data/weather/{location}",
+    method: 'GET',
+    path: '/api/v1/data/weather/{location}',
     handler: WeatherData.getForecast,
     config: {
       cache: {
-        expiresIn: 86400000
+        expiresIn: 86400000,
       },
       validate: {
         params: {
           location: Joi.string()
             .valid(
-              "albany",
-              "baldivis",
-              "broome",
-              "bunbury",
-              "cannington",
-              "carlisle",
-              "collie",
-              "east-perth",
-              "geraldton",
-              "kalgoorlie",
-              "karratha",
-              "leonora",
-              "marble-bar",
-              "meekatharra",
-              "mount-magnet",
-              "perth",
-              "southern-cross"
+              'albany',
+              'baldivis',
+              'broome',
+              'bunbury',
+              'cannington',
+              'carlisle',
+              'collie',
+              'east-perth',
+              'geraldton',
+              'kalgoorlie',
+              'karratha',
+              'leonora',
+              'marble-bar',
+              'meekatharra',
+              'mount-magnet',
+              'perth',
+              'southern-cross'
             )
-            .required()
-        }
+            .required(),
+        },
       },
-      description: "Updates",
-      notes: "Weather updates from Open Weather Map.",
-      tags: ["api", "Weather"]            
-    }
-  }
+      description: 'Updates',
+      notes: 'Weather updates from Open Weather Map.',
+      tags: ['api', 'Weather'],
+    },
+  },
 ];
--- src/methods/index.js
+++ src/methods/index.js
@@ -1,25 +1,25 @@
 // Methods for retreving a single document.
-const employee = require("./retrieve/employee");
-const manager = require("./retrieve/manager");
-const team = require("./retrieve/team");
+const employee = require('./retrieve/employee');
+const manager = require('./retrieve/manager');
+const team = require('./retrieve/team');
 
 // Methods for retreving a collection of documents.
-const employees = require("./collection/employees");
-const paginated = require("./collection/paginated");
-const surname = require("./collection/surname");
+const employees = require('./collection/employees');
+const paginated = require('./collection/paginated');
+const surname = require('./collection/surname');
 
 // Methods for inserting the datas.
-const avatar = require("./avatars/insert");
-const getAvatar = require("./avatars/get");
-const missing = require("./avatars/missing");
-const insert = require("./collection/insert");
-const remove = require("./collection/remove");
+const avatar = require('./avatars/insert');
+const getAvatar = require('./avatars/get');
+const missing = require('./avatars/missing');
+const insert = require('./collection/insert');
+const remove = require('./collection/remove');
 
 // Statistics Methods
-const stats = require("./statistics");
+const stats = require('./statistics');
 
 // Organisation Methods
-const organisation = require("./organisation");
+const organisation = require('./organisation');
 
 module.exports = [].concat(
   avatar,

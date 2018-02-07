7829fb18e6b6dae74b18c1f4bd9817d488a99841
Drew Robinson <drew.robinson@gmail.com>
2018-02-07 11:37:36 +0800

    Correct reply() error and add neater Boom error handler if employee not found

--- src/handlers/census/employee/get/getEmployee.js
+++ src/handlers/census/employee/get/getEmployee.js
@@ -1,7 +1,11 @@
+import Boom from 'boom';
+
 export default function getEmployee(request, reply) {
   const { employeeID } = request.params;
   request.server.methods.db.getEmployee(employeeID, (error, employee) => {
-    if (error) reply(error).code(500);
+    if (error) {
+      return reply(Boom.notFound(`No staff member found for user ID "${employeeID}"`));
+    }
     return reply(employee).code(200);
   });
 }
